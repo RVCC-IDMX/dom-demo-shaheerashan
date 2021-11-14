6cb167d77df3bfd106aa4a1158c4756926390f60
shaheerashan <77646573+shaheerashan@users.noreply.github.com>
2021-11-14 17:40:48 -0500

    Dom demo passed

--- main.js
+++ main.js
@@ -6,3 +6,58 @@ https://youtu.be/hdI2bqOjy3c?t=4228
 
 Start at 1:10:29
  */
+
+// Single element
+// const form = document.getElementById('my-form');
+// console.log(form);
+// console.log(document.querySelector('h1')); // Will only select the fist h1
+
+// Multiple element
+// console.log(document.querySelectorAll('.item'))
+// console.log(document.getElementsByClassName('item'));
+// console.log(document.getElementsByTagName('li'));
+
+// const items = document.querySelectorAll('.item')
+// items.forEach((item) => console.log(item));
+// const ul = document.querySelector('.items');
+// ul.remove();
+// ul.lastElementChild.remove();
+// ul.firstElementChild.textContent = 'Hello';
+// ul.children[1].innerText = 'Brad';
+// ul.lastElementChild.innterHTML = '<h1>Hello</h1>';
+// const btn = document.querySelector('.btn');
+// btn.style.background = 'red';
+// btn.addEventListener('click', (e) => { // can also mouseover, mouseout, etc
+//     e.preventDefault();
+//     document.querySelector('#my-form').style.background = '#ccc';
+//     document.querySelector('body').classList.add('bg-dark');
+//     document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>';
+// });
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();
+
+    if(nameInput.value === '' || emailInput.value === '') {
+        // alert
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter all fields';
+
+        setTimeout(() => msg.remove(), 3000);
+    } else {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+        userList.appendChild(li);
+
+        // Clear fields
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+}
